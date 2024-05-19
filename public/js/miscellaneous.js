f3bf93cbf0d916c06132b8191c7478121db14636
D2060 <d20601525@gmail.com>
2024-05-19 19:22:42 +0530

    added auto fetching year for footer

--- public/js/miscellaneous.js
+++ public/js/miscellaneous.js
@@ -1,3 +1,9 @@
 document.getElementById('BookSwapHub').addEventListener('click', function() {
     window.location.reload();
 });
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Update the span element with the current year
+document.getElementById('current-year').textContent = currentYear;

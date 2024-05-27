db3baab0b3e30fc5dd341cf1685f3766368c9f83
D2060 <d20601525@gmail.com>
2024-05-27 14:11:00 +0530

    added chat with us icon

--- public/js/miscellaneous.js
+++ public/js/miscellaneous.js
@@ -7,3 +7,13 @@ const currentYear = new Date().getFullYear();
 
 // Update the span element with the current year
 document.getElementById('current-year').textContent = currentYear; 
+
+// script.js
+function toggleContact() {
+    const contactModal = document.getElementById('contact-modal');
+    if (contactModal.style.display === "none" || contactModal.style.display === "") {
+        contactModal.style.display = "flex";
+    } else {
+        contactModal.style.display = "none";
+    }
+}
